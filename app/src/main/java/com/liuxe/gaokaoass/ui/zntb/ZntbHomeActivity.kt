abf824aad5c1bf7c65ed38cdf6169d74276369c7
liuxe <935531860@qq.com>
2020-04-30 16:10:48 +0800

    智能填报activity

---
+++ app/src/main/java/com/liuxe/gaokaoass/ui/zntb/ZntbHomeActivity.kt
@@ -0,0 +1,13 @@
+package com.liuxe.gaokaoass.ui.zntb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.liuxe.gaokaoass.R
+
+class ZntbHomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_zntb_home)
+    }
+}
